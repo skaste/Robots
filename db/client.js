96794f6a3ac5711fc1490cd9551b47e09bca3dff
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-12-04 15:58:15 -0600

    creating seed.js pseudocode for the functions that will need to be completed

---
+++ db/client.js
@@ -0,0 +1,5 @@
+const { Client } = require("pg");
+
+
+
+module.exports = client;
---
+++ db/seed.js
@@ -0,0 +1,28 @@
+const client = require('./client');
+
+//drop tables for robots, task, and customers
+
+//build tables for robots, task, and customers
+
+ //async await function with try catches because they call api:
+
+// CREATE TABLE robots ('');
+// CREATE TABLE tasks ('');
+// CREATE TABLE customers ('');
+
+
+//create initial data for robots, task, and customers
+
+// create an async await function with try catch and awaits client.query:
+
+// INSERT INTO robots ('name, model, company_name, longevity_months, is_child_safe, cost, release_date')
+// VALUES ('same thing as insert into but each gets '' around it);
+
+// return values from the inserted data set
+
+//build all tables and create initial data
+
+module.exports = {
+  //export my {object}
+};
+
