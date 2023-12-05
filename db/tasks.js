eac89181762085ad0ab583ac6442691dfde30456
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-12-05 14:08:40 -0600

    working on function for creating task in task.js

---
+++ db/tasks.js
@@ -0,0 +1,12 @@
+const client = require("./client");
+
+const createTasks = async (name, description) => {
+  try {
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+module.exports = {
+  createTasks,
+};
