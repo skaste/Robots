76c6d4e705fe68d18b83dbcdce4c3ad57480bd0b
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-12-05 22:49:36 -0600

    created a function to get robots by id

--- api/robots.cjs
+++ api/robots.cjs
@@ -10,4 +10,16 @@ router.get("/", async (req, res, next) => {
     next(err);
   }
 });
+
+//get api/robots by id
+router.get("/:id", async (req, res, next) => {
+  try {
+    const robots = await getRobotById(req.params.id);
+    res.send(routine);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+});
+
 module.exports = router;
